a11e31476e8b156e9dce6bdb281ceb32371a288d
bin7096 <bin_id@163.com>
2019-01-16 18:18:06 +0800

    2019-01-16

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,5 +16,8 @@ Page({
         var styleObj1 = obj1 = progressBar.init(this, 'pureColorAnnular', 30, 50, 'rgb(146,80,58)', '#3399FF', 'progressBar1', 100, 0);
         var styleObj2 = obj2 = progressBar.init(this, 'pureColorAnnular', 60, 30, '#CCC', '#3399FF', 'progressBar2', 200, 1);
         var styleObj3 = obj3 = progressBar.init(this, 'pureColorAnnular', 90, 30, '#CCC', 'rgb(146,80,58)', 'progressBar3', 300, 2);
+    },
+    add: function (e) {
+        console.log(e);
     }
 })
